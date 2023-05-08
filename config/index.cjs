721043294681121ea0d130eeecf6a58549c9ea67
tao <63838655+Taoyao-TT@users.noreply.github.com>
2023-05-08 20:27:11 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,50 +8,84 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wxa4b64d37556ef43e',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '4c59b411b5048b5aa5bc1a715d319e0e',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '山西',
+  CITY: '临汾',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '戎戎',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+     // id: "oj3u45izy01N9q6Nlywgie8U0loE",
+       id: "oj3u45hFA1SAgKuh0-SE_Lpf7JLs",
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'r7FDgQSLI2I1Fzm_XNSHvdondlJ-snB9ujjdwbPZWX8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '02-03',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '戎戎', year: '2002', date: '12-01',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '*生日', name: '涛涛', year: '2000', date: '03-10',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相恋纪念日', year: '2022', date: '10-19',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-10-19' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
+    {
+      // 想要发送的人的名字
+      name: '戎戎1',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+       id: "oj3u45izy01N9q6Nlywgie8U0loE",
+       //id: "oj3u45hFA1SAgKuh0-SE_Lpf7JLs",
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'r7FDgQSLI2I1Fzm_XNSHvdondlJ-snB9ujjdwbPZWX8',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '02-03',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '戎戎', year: '2002', date: '12-01',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '*生日', name: '涛涛', year: '2000', date: '03-10',
+        },
+        {
+          type: '节日', name: '相恋纪念日', year: '2022', date: '10-19',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-10-19' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
 
+  
+   
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'HyGrnmWaKCE6Hh-nbrhdbR_sbSJQ-cTT7GM7ihdTNw0',
 
   CALLBACK_USERS: [
     {
@@ -61,7 +95,7 @@ const USER_CONFIG = {
     }
   ],
 
+
 }
 
 module.exports = USER_CONFIG
-
